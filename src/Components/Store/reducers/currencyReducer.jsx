6d53314bfa2866206e0741fd8cbb3939a31d4de8
Gustavo Rotta <gustavo.rotta@hotmail.com>
2022-01-10 17:49:23 -0300

    MissionComplete

--- src/Components/Store/reducers/currencyReducer.jsx
+++ src/Components/Store/reducers/currencyReducer.jsx
@@ -1,11 +1,14 @@
 import { GET_CURRENCY } from "../actions";
 
-const initialState = "USD";
+const initialState = "";
 
 const currencyReducer = (state = initialState, action) => {
     switch (action.type) {
         case GET_CURRENCY:
             return action.payload.currency;
+
+        case "persist/REHYDRATE":
+            return "USD";
 
         default:
             return state;
